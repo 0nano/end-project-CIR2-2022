7bcda90f59a10b1752b993c50f15ec87b38f9c63
0nano <35236525+0nano@users.noreply.github.com>
2022-06-14 14:28:21 +0200

    changement interface de développement

---
+++ ressources/config.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Basic parameter of postgresql
+ */
+const DB_SERVER = '127.0.0.1';
+const DB_PORT = '5432';
+const DB_NAME = 'db_fysm'; // fysm = find your sports match
+const DB_USER = 'postgres';
+const DB_PASSWORD = 'new_password';
+
+?>
---
+++ ressources/database.php
@@ -0,0 +1,24 @@
+<?php
+    require_once 'config.php';
+
+    class Database 
+    {
+        protected $PDO;
+
+        /**
+         * Construct the connection to the PostgreSQL database.
+         * 
+         * @throws PDOException Error thrown if the connection to the database failed.
+         */
+        public function __construct()
+        {
+            $db_name = DB_NAME;
+            $db_server = DB_SERVER;
+            $db_port = DB_PORT;
+
+            $dsn = "pgsql:dbname={$db_name};host={$db_server};port={$db_port}";
+
+            $this->PDO = new PDO($dsn, DB_USER, DB_PASSWORD);
+        }
+    }
+?>
