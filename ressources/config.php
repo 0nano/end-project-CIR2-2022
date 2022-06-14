4b23bb5a9d30405a054fa55d8a78b6fb10765727
0nano <35236525+0nano@users.noreply.github.com>
2022-06-14 16:40:50 +0200

    test

--- ressources/config.php
+++ ressources/config.php
@@ -6,6 +6,6 @@ const DB_SERVER = '127.0.0.1';
 const DB_PORT = '5432';
 const DB_NAME = 'db_fysm'; // fysm = find your sports match
 const DB_USER = 'fysm';
-const DB_PASSWORD = 'new_password';
+const DB_PASSWORD = 'fysmpass';
 
 ?>
