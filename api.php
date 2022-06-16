a71ae1a9b2eddd758357f42cc0dc1475ded17ef7
0nano <35236525+0nano@users.noreply.github.com>
2022-06-16 09:22:00 +0200

    debut d'api

--- api.php
+++ api.php
@@ -1,5 +1,65 @@
 <?php
+require_once 'resources/config.php';
+require_once 'resources/database.php';
+require_once LIBRARY_PATH . '/common.php';
+require_once LIBRARY_PATH . '/exceptions.php';
 
-phpinfo();
+$pathInfo = explode('/', trim($_SERVER['PATH_INFO'], '/\\'));
+
+header('content-type: application/json; charset=utf-8');
+
+$db = new Database();
+
+function getAuthorizationToken(): ?string{
+    
+	$authorization = $_SERVER['HTTP_AUTHORIZATION'];
+
+	if (!isset($authorization)) {
+		APIErrors::invalidHeader();
+	}
+
+	$authorization = explode(' ', trim($authorization), 2)[1];
+
+	if (empty($authorization)) {
+		APIErrors::invalidGrant();
+	}
+	return $authorization;
+}
+
+class APIErrors{
+
+	public static function invalidGrant()
+	{
+		http_response_code(400);
+		die(json_encode(array(
+			'error' => 'invalid_grant',
+			'error_description' => 'The authorization code is invalid or expired.'
+		)));
+	}
+
+	public static function invalidHeader()
+	{
+		http_response_code(400);
+		die(json_encode(array(
+			'error' => 'invalid_header',
+			'error_description' => 'The request is missing the Authorization header or the Authorization header is invalid.'
+		)));
+	}
+
+	public static function invalidRequest()
+	{
+		http_response_code(400);
+		die(json_encode(array(
+			'error' => 'invalid_request',
+			'error_description' => 'The request is missing a parameter, uses an unsupported parameter, uses an invalid parameter or repeats a parameter.'
+		)));
+	}
+
+	public static function internalError()
+	{
+		http_response_code(500);
+		die();
+	}
+}
 
 ?>
