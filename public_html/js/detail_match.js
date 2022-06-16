71706adfc8d63c6a79eb61ab882ae49be8818e6b
clementcaen <clement.jaminion@gmail.com>
2022-06-16 11:13:25 +0200

    correction requête ajax GET match

---
+++ public_html/js/detail_match.js
@@ -0,0 +1,3 @@
+function detail_match() {
+    
+}
--- public_html/js/explore.js
+++ public_html/js/explore.js
@@ -6,9 +6,10 @@ function print_result(matchs) {
     if (!matchs[0]){
         content.append("<p class='alert alert-secondary'>Aucun matchs n'a été trouvé</p>")
     }else{
+        content.append("<div id='matchs' class='col-md-5'></div>");
         matchs.forEach(function (match) {
-           content.append("" +
-               "<div class='match card'>" +
+            let a_match_content =
+               "<div id='"+ match.id +"' class='match card'>" +
                "    <div class='card-body'>" +
                "        <h5 class='card-title'>"+ match.sport_name +"</h5>" +
                "        <h6 class='card-subtitle'>"+match.organizer.firstname + " " + match.organizer.lastname +"</h6>" +
@@ -17,16 +18,24 @@ function print_result(matchs) {
                "        <p class='card-text city'>"+match.city+"</p>"+
                "        <p class='card-text nb_player_max'>"+match.max_player+"</p>"+
                "        <p class='card-text nb_registered'>"+match.nb_regis+"</p>"+
-               "    </div> " +
-               "</div>");
+               "    </div> "+
+               "</div>";
+            content.querySelector("#matchs").append(a_match_content);
+            listener_match(a_match_content, match.id);
         });
         map();
     }
 }
-
-
-
-
+function listener_match(match, id_match) {
+    match.addEventListener("click", function (evt) {
+        evt.preventDefault();
+        $.ajax({
+            method: "GET",
+            url: "api.php/detail?id_match="+id_match,
+            success: detail_match
+        });
+    });
+}
 
 function explore(matchs) {
     print_result(matchs);
