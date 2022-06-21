5986fa2a3925289ece2a131f29d5fb79be86a874
clementcaen <clement.jaminion@gmail.com>
2022-06-21 16:13:27 +0200

    test database search

--- api.php
+++ api.php
@@ -215,7 +215,8 @@ switch ($pathInfo[0] . $_SERVER['REQUEST_METHOD']) {
 			$period = $_GET['period'];
 			$completeIncomplete = $_GET['match'];
 			echo $city . $sport . $period .$completeIncomplete;
-			return $db->searchMatch($period, $sport, $city, $completeIncomplete);
+			echo $db->searchMatch($period, $sport, $city, $completeIncomplete);
+
 		} catch (Exception $_){
 			APIErrors::internalError();
 		}
