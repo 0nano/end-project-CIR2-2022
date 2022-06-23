af021109c5f9cdc3bb5643347777f2f9f6624bdb
clementcaen <clement.jaminion@gmail.com>
2022-06-23 14:03:34 +0200

    test

--- api.php
+++ api.php
@@ -173,7 +173,7 @@ switch ($pathInfo[0] . $_SERVER['REQUEST_METHOD']) {
 		try {
 			if ($_PUT["pwd_verif"] === $_PUT["pwd"]) {
 				$authorization = getAuthorizationToken();
-				$picture = file_get_contents($_PUT['photo']);
+				$picture = ($_PUT['photo']);// file_get_contents
 
 				$email = $db->getUserInfos($authorization)["email"];
 				die(json_encode($db->modifyAccount($authorization, (int) $_PUT["age"], $_PUT["city"], $picture, $_PUT["pwd"], (int) $_PUT["shape"], $email)));
