de023aec42b64e07e515049d91024b8773561204
clementcaen <clement.jaminion@gmail.com>
2022-06-23 15:09:02 +0200

    ville dans profile.js

--- public_html/js/profile.js
+++ public_html/js/profile.js
@@ -36,7 +36,7 @@ function profile() {
                         past.innerHTML +=  "<div class='card text-dark mt-3'>" +
                         "       <div class='card-header'><h6 class='card-title'>Macth du "+ heure.toLocaleDateString('fr-FR', options) + " à "+ heure.getHours() +":"+ heure.getMinutes() +
                         "<span class='badge rounded-pill text-bg-primary float-end' id='"+ element.id +"'>Satut : "+ status +"</span></h6></div>"+
-                        "       <div class='card-body'><p class='card-text'>Sport: "+ element.sport_name +"<br>Dans la ville de "+ element.city +"</p>"+
+                        "       <div class='card-body'><p class='card-text city'>Sport: "+ element.sport_name +"</p><p class='card-text city'>"+element.city +"</p>"+
                         "<button class='btn btn-success btn_submit float-end' name='viewMatch' matchID='"+ element.id +"'>Voir les détails du match</button></div>"+
                         "    </div>";
                         set_match.push(element.id);
@@ -46,7 +46,7 @@ function profile() {
                         futur.innerHTML += "" + "<div class='card text-dark mt-3'>" +
                         "       <div class='card-header'><h6 class='card-title'>Macth du "+ heure.toLocaleDateString('fr-FR', options) + " à "+ heure.getHours() +":"+ heure.getMinutes() +
                         "<span class='badge rounded-pill text-bg-primary float-end' id='"+ element.id +"'>Satut : "+ status +"</span></h6></div>"+
-                        "       <div class='card-body'><p class='card-text'>Sport: "+ element.sport_name +"<br>Dans la ville de "+ element.city +"</p>"+
+                        "       <div class='card-body'><p class='card-text '>Sport: "+ element.sport_name +"</p><p class='card-text city'>"+element.city +"</p>"+
                         "<button class='btn btn-success btn_submit float-end' name='viewMatch' matchID='"+ element.id +"'>Voir les détails du match</button></div>"+
                         "    </div>";
                         set_match.push(element.id);
@@ -56,18 +56,24 @@ function profile() {
                 }
             });
             content.innerHTML = "" + futur.outerHTML + past.outerHTML;
-
+            let cities_divs = document.getElementsByClassName("city");
+            cities_divs.forEach( function (a_city_div) {
+                find_city_la_poste(a_city_div.textContent).then(function (result) {
+                    a_city_div.textContent = "Dans la ville de " + result;
+                });
+            });
             change_status.forEach(element => {
                 document.getElementById(element).innerText = "Statut : Organisateur et Joueur";
             })
 
             let viewbutttons = document.getElementsByName('viewMatch');
             viewbutttons.forEach(element => {
-                element.addEventListener('click', (event) => {
+                listener_match(element, element.getAttribute('matchID'));
+                /*element.addEventListener('click', (event) => {
                     event.stopImmediatePropagation();
-                    console.log('viewMatch '+ element.getAttribute('matchid'));
-                    detail_match(element.getAttribute('matchid'));
-                })
+                    console.log('viewMatch '+ element.getAttribute('matchID'));
+                    detail_match(element.getAttribute('matchID'));
+                })*/
             })
         }
     });
