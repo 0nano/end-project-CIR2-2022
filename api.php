8cbe2be19e5784041f427e9664cd2d383337d9ae
clementcaen <clement.jaminion@gmail.com>
2022-06-23 13:47:13 +0200

    petit test put à la place de $_FILES 4

--- api.php
+++ api.php
@@ -173,8 +173,9 @@ switch ($pathInfo[0] . $_SERVER['REQUEST_METHOD']) {
 		try {
 			if ($_PUT["pwd_verif"] === $_PUT["pwd"]) {
 				$authorization = getAuthorizationToken();
+				echo "photo blob: " . $_PUT['photo'];
 				$picture = file_get_contents($_PUT['photo']);
-				echo $picture;
+
 				$email = $db->getUserInfos($authorization)["email"];
 				die(json_encode($db->modifyAccount($authorization, (int) $_PUT["age"], $_PUT["city"], $picture, $_PUT["pwd"], (int) $_PUT["shape"], $email)));
 			}else{
