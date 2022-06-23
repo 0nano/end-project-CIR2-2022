d8fb4e809878080ec523803b38998aba7aa46f92
clementcaen <clement.jaminion@gmail.com>
2022-06-23 09:18:29 +0200

    Detail match, affichage ville

--- public_html/js/api_city.js
+++ public_html/js/api_city.js
@@ -7,7 +7,7 @@ async function find_city_la_poste(insee){
                 insee +
                 "&lang=fr&facet=code_commune_insee&apikey=cb4836a947a7bc6c9a6a22f6ad66ba0d0fca3cfe6825608f4689a5a8",
         }).done( function (city) {
-            let result = city.records[0]["fields"]["nom_de_la_commune"];
+            result = city.records[0]["fields"]["nom_de_la_commune"];
         });
         return new Promise((resolve) => {
             if (result) {
